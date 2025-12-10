6f7d42d59a0c7c2e23a377aa2ee4124cef313db2
24DP3NLaci <A240675NL@rvt.lv>
2025-12-10 15:26:05 +0200

    chapter19

---
+++ src/main/java/rvt/chapter19.java
@@ -0,0 +1,24 @@
+package rvt;
+
+public class chapter19 {
+
+    public static void main(String[] args) {
+        int number = 5;
+        if (number < 5){
+        System.out.println(number);
+        number++;
+    }
+    }
+
+
+    int number = 100;{
+    while (true) {
+        if(number == 100){
+            System.out.println("Darbība tika apstādināta");
+            break;
+        }
+    }
+}
+
+}
+

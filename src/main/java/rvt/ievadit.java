04f53423e0f56d9cd2eb062e4deddbf4d6437f39
24DP3NLaci <A240675NL@rvt.lv>
2025-12-10 15:28:42 +0200

    Uzdevums, kad ievada noteiktu ciparu programma beidzās.

---
+++ src/main/java/rvt/ievadit.java
@@ -0,0 +1,19 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class ievadit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+while(true){
+        System.out.println("Ievadi ciparu: ");
+if(scanner.nextInt() == 5){
+System.out.println("Programma beidzas jo tiek ievadits skaitlis 5.");
+break;
+}
+continue;
+}
+    }
+
+}

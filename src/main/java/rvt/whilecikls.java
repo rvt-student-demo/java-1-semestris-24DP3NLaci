bc610293ad512e78192a95803e78e4a2668078c7
24DP3NLaci <A240675NL@rvt.lv>
2025-12-10 15:32:56 +0200

    uzdevums ar while ciklu, kur programma apturas pec 5 cikliem.

---
+++ src/main/java/rvt/whilecikls.java
@@ -0,0 +1,24 @@
+package rvt;
+import java.util.Scanner;
+
+public class whilecikls {
+    public static void main(String[] args) {
+int cip = 0;
+
+        while(true){
+            cip++;
+            System.out.println(cip);
+
+            if(cip == 5){
+                System.out.println("Cikls tiek beigts jo ir pagajusi 5 cikli");
+                break;
+            }
+            continue;
+        }
+       
+
+            
+                    
+    }
+}
+

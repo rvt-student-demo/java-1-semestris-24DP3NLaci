5db6b2d8e6a9361f1c2beb52a76f3ac2ec939453
24DP3NLaci <A240675NL@rvt.lv>
2025-12-10 15:31:58 +0200

    Velviens sequence uzdevums

---
+++ src/main/java/rvt/sequence2.java
@@ -0,0 +1,28 @@
+package rvt;
+import java.util.Scanner;
+public class sequence2 {
+public static void main(String[] args) {    
+Scanner scanner = new Scanner(System.in);
+int Pirmais = 0;
+int i = 0;
+int kopa = 0;
+
+System.out.println("Ievadi pirmo skaitli: ");
+Pirmais = scanner.nextInt();
+
+
+
+for(i = Pirmais; i == Pirmais; i++ ) {
+
+    i *= i;
+ 
+
+}
+System.out.println("Rezultats ir: " + kopa);
+scanner.close();
+
+
+
+}
+}
+
